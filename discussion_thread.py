995ed54b8a39b34fea4d2b1691320d85dc408f99
Abhi Agarwal <goldcobrapro@gmail.com>
2017-11-12 11:38:46 -0500

    Make into a Python Class

--- discussion_thread.py
+++ discussion_thread.py
@@ -1,77 +1,51 @@
+"""dicussion thread"""
 import praw
-import datetime
-import time
-import sys
-import os
 
-def getLatestDThread(NE):
-    """
-    Returns latest discussion thread object
-    """
+class DiscussionThread(object):
+    """handles discussion thread"""
+    def __init__(self: DiscussionThread, reddit: praw.Reddit, subreddit: str,
+                 new_duration: int = 24, comments_limit: int = 300) -> None:
+        self.reddit: praw.Reddit = reddit
+        self.subreddit: praw.models.Subreddit = self.reddit.subreddit(subreddit)
+        self.duration = new_duration
+        self.limit = comments_limit
 
-    d_threads = [x for x in NE.search('Discussion Thread', time_filter='week') if 'Discussion' in x.title]
+    def update(self: DiscussionThread) -> bool:
+        """updates discussion thread if necessary"""
+        thread: praw.reddit.models.Submission = self.latest()
+        if self.needs_new(thread):
+            self.post(thread)
+        return True
 
-    dateref = datetime.datetime.now() - datetime.timedelta(weeks = 1)
+    def get_body(self: DiscussionThread) -> str:
+        """gets body from wiki page"""
+        return self.subreddit.wiki["dt/config"].content_md
 
-    for x in d_threads:
+    def post(self: DiscussionThread, old: praw.models.Submission) -> bool:
+        """posts the discussion thread"""
+        old_moderation: praw.models.reddit.submission.SubmissionModeration = old.mod
+        old_moderation.sticky(state=False)
 
-        if datetime.datetime.utcfromtimestamp(x.created_utc) > dateref:
-            latest = x
-            dateref = datetime.datetime.utcfromtimestamp(x.created_utc)
+        body: str = self.get_body()
+        new: praw.models.Submission = self.subreddit.submit(
+            "Discussion Thread", selftext=body, url=None, resubmit=True, send_replies=False)
+        new_moderation: praw.models.reddit.submission.SubmissionModeration = new.mod
+        new_moderation.sticky(state=True, bottom=False)
+        new_moderation.distinguish()
+        new_moderation.suggested_sort(sort='new')
+        return True
 
-    return latest
+    def latest(self: DiscussionThread) -> praw.models.Submission:
+        """returns the latest discussion thread"""
+        for submission in self.subreddit.search("Discussion Thread", sort="new"):
+            if submission.author == self.reddit.user.me():
+                return submission
 
+    def needs_new(self: DiscussionThread, thread: praw.models.Submission) -> bool:
+        """checks if new discussion thread is needed"""
+        import datetime
+        time: datetime.timedelta = (datetime.datetime.utcnow() -
+                                    datetime.datetime.utcfromtimestamp(thread.created_utc))
+        hours: int = int(time.total_seconds() / 3600)
 
-def checkDThread(dthread, comment_limit, age_limit):
-    """
-    Returns true if latest discussion thread is completed and a
-    new one is required
-    """
-
-    # hours that the discussion thread has been up
-    delta = (datetime.datetime.utcnow() - datetime.datetime.utcfromtimestamp(dthread.created_utc))
-    delta_hours = delta.total_seconds()/3600
-
-    # number of comments in the discussion thread
-    num_comments = dthread.num_comments
-
-    return (num_comments > comment_limit or delta_hours > age_limit)
-
-
-def postNewDThread(old_dthread, NE):
-    old_dthread_moderation = praw.models.reddit.submission.SubmissionModeration(old_dthread)
-    old_dthread_moderation.sticky(state=False)
-    time.sleep(5)
-    post_text = old_dthread.selftext
-
-    new_dthread = NE.submit("Discussion Thread", selftext= post_text, url=None, resubmit=True, send_replies=False)
-    new_dthread_moderation = praw.models.reddit.submission.SubmissionModeration(new_dthread)
-    time.sleep(5)
-    new_dthread_moderation.sticky(state=True, bottom=False)
-    new_dthread_moderation.distinguish()
-    new_dthread_moderation.suggested_sort(sort='new')
-
-
-def updateSticky(comment_limit, hours_limit, NE):
-    '''
-    Updates sticky if necessary
-    '''
-    print("getting latest thread")
-    x = getLatestDThread(NE)
-    time.sleep(10)
-    print("checking thread")
-    if checkDThread(x, comment_limit, hours_limit):
-        print("posting thread...")
-        postNewDThread(x, NE)
-        print("Posted new thread")
-    else:
-        print("Current thread is sufficient")
-
-
-## Main
-
-r = praw.Reddit()
-NE = r.subreddit("neoliberal")
-
-updateSticky(300, 5, NE)
-
+        return thread.num_comments > self.limit or hours > self.duration
