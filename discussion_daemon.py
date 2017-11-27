ee5d9fb55780cbe57978820b06c847414be868b1
Abhi Agarwal <goldcobrapro@gmail.com>
2017-11-26 21:55:10 -0500

    Update to latest changes

--- discussion_daemon.py
+++ discussion_daemon.py
@@ -1,20 +1,30 @@
 """turns discussion thread into daemon"""
 import daemon
-import praw
 
 from discussion_thread import DiscussionThread
 
 def main() -> None:
     """main function"""
-    reddit: praw.Reddit = praw.Reddit()
-    thread: DiscussionThread = DiscussionThread(reddit, "neoliberal")
+    import os
+    import praw
+    reddit: praw.Reddit = praw.Reddit(
+        client_id=os.environ["client_id"],
+        client_secret=os.environ["client_secret"],
+        refresh_token=os.environ["refresh_token"],
+        user_agent="linux:discussion_thread:v2.0 (by /u/CactusChocolate)"
+    )
+    thread: DiscussionThread = DiscussionThread(
+        reddit,
+        "neoliberal",
+        os.environ["slack_webhook_url"]
+    )
 
     with daemon.DaemonContext(
         working_directory="/var/lib/discussion_thread",
         umask=0o002,
         pidfile=daemon.pidfile.TimeoutPIDLockFile("/var/run/discussion_thread.pid")
         ):
-        thread.check_post()
+        thread.check()
         from time import sleep
         sleep(30)
 
