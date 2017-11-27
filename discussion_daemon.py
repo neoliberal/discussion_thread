c0cc84c9fd8b2a267fc8b4fef904e54027cf1c21
Abhi Agarwal <goldcobrapro@gmail.com>
2017-11-26 22:07:30 -0500

    quick debugging

--- discussion_daemon.py
+++ discussion_daemon.py
@@ -11,19 +11,21 @@ def main() -> None:
         client_id=os.environ["client_id"],
         client_secret=os.environ["client_secret"],
         refresh_token=os.environ["refresh_token"],
-        user_agent="linux:discussion_thread:v2.0 (by /u/CactusChocolate)"
+        user_agent="linux:neoliberal_discussion_thread:v2.0 (by /u/CactusChocolate)"
     )
+    print("authenticated")
     thread: DiscussionThread = DiscussionThread(
         reddit,
         "neoliberal",
         os.environ["slack_webhook_url"]
     )
-
+    print("created object")
     with daemon.DaemonContext(
         working_directory="/var/lib/discussion_thread",
         umask=0o002,
         pidfile=daemon.pidfile.TimeoutPIDLockFile("/var/run/discussion_thread.pid")
         ):
+        print("in daemon")
         thread.check()
         from time import sleep
         sleep(30)
