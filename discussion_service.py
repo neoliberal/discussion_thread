067166d84a887282d86fe617e940e742a04337dc
Abhi Agarwal <goldcobrapro@gmail.com>
2017-11-27 21:00:01 -0500

    Switch to systemd

--- discussion_service.py
+++ discussion_service.py
@@ -1,16 +1,14 @@
 """turns discussion thread into daemon"""
 import logging
+import os
 import sys
 
-import daemon
-from daemon import pidfile
+import praw
 
 from discussion_thread import DiscussionThread
 
 def main() -> None:
     """main function"""
-    import os
-    import praw
     reddit: praw.Reddit = praw.Reddit(
         client_id=os.environ["client_id"],
         client_secret=os.environ["client_secret"],
@@ -23,7 +21,7 @@ def main() -> None:
         "neoliberal",
         os.environ["slack_webhook_url"]
     )
-    file_handler: logging.Handler = logging.FileHandler("/var/log/flairbot.log")
+    file_handler: logging.Handler = logging.FileHandler("/var/log/discussion_thread.log")
     format_string: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     file_handler.setFormatter(logging.Formatter(format_string))
     file_handler.setLevel(logging.DEBUG)
@@ -38,17 +36,10 @@ def main() -> None:
 
     sys.excepthook = log_unhandled
 
-    with daemon.DaemonContext(
-        working_directory="/var/lib/discussion_thread",
-        umask=0o002,
-        pidfile=pidfile.TimeoutPIDLockFile(
-            "/var/run/discussion_thread.pid")
-    ) as context:
-        while context.is_open:
-            thread.check()
-            from time import sleep
-            sleep(30)
-
+    while True:
+        thread.check()
+        from time import sleep
+        sleep(5)
 
 if __name__ == "__main__":
     main()
